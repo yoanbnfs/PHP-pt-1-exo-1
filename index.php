92ce885de7e4f55f1c4849a0c65aef966433b47e
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-13 09:01:23 +0100

    ajout de l'html

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-  $nom = "Yoan";
-  echo $nom;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exo-1</title>
+  </head>
+  <body>
+    <?php
+      $name = 'Yoan';
+      echo $name;
+    ?>
+  </body>
+</html>
